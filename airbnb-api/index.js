2dc41b60689abc9ab20a01a153e8bc20cd16e4f2
Santiago Velez <svy9602@hotmail.com>
2020-09-09 18:49:48 -0500

    Se realiza api en node

---
+++ airbnb-api/index.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const app = express()
+app.use(express.json())
+//urlencoded es para cuando llegue parametros por las rutas se puedan leer bien
+app.use(express.urlencoded({ extended: false }))
+const port = 3001
+
+//Routes
+const experienciesRoutes = require('./routes/experiences')
+
+app.use('/experiences', experienciesRoutes)
+
+/* Sample endpoints:
+    - localhost:3000/experiences/
+    - localhost:3000/experiences/top5
+    - localhost:3000/experiences/detail/1..n
+*/
+
+app.listen(port, () => {
+    console.log(`Se inicia el servidor en el puerto ${port}`)
+})
